4a41f8e9a99b767ba7d96375f7cc0062f0b0af40
agarmir0920 <tsarevantuan@yandex.ru>
2022-03-25 21:58:09 +0300

    Written modules and started debugging

--- lab_02/src/Interpol.hs
+++ lab_02/src/Interpol.hs
@@ -1,85 +1,121 @@
 module Interpol (interpolation) where
 
-import Table (Coor, Coors, CoorsTable)
+import CoorsTable (Var, Coors, CoorsTable)
 
 type Degree = Int
 type Degrees = [Degrees]
 
-type CoorsList = [Coor]
-type SymmCoorsLists = [CoorsList]
+type VarsList = [Var]
+type VarsLists = [VarsList]
 
-type Index = Int
+type CoefsList = [Double]
 
-getSymmCoorsList :: CoorsList -> Coor -> Degree -> CoorsList
-getSymmCoorsList [] _ _ = []
-getSymmCoorsList lst coor degree
-    | degree < 0 = if len > 1 && diff > lstStep then [] else lst
-    | midElem > coor = getSymm(init lst, coor, degree - 1)
-    | otherwise = getSymm(tail lst, coor, degree - 1)
-    where diff = abs $ midElem - coor
-          len = length lst
-          midElem = lst!!midInd
-          midInd = div $ len 2
+type ValuesList = [Double]
 
-getSymmCoorsLists :: CoorsTable -> Coors -> Degrees -> SymmCoorsLists
-getSymmCoorsLists [_] [] [_] = []
-getSymmCoorsLists table coors degrees
-    | null symmLst = []
-    | otherwise = symmLst : nextLsts
-    where symmLst = getSymmCoorsList $ coorsLst coors!!0 degrees!!0
-          nextLsts = getSymmCoorsLists (table, tail coors, tail degrees)
-          coorsLst = nub $ table!!ind
-          ind = length (table - length (coors) - 1)
+-- Создание списка разделенных разностей функции
+getVarsDiffsLst :: VarsLists -> VarsLists
+getVarsDiffsLst [] = []
+getVarsDiffsLst varsLsts = diff : getVarsDiffsLst perfVarsLsts
+    where diff = (y1 - y2) / (x1 - x2)
+          y1 = head ys
+          y2 = ys !! 1
+          x1 = head xs
+          x2 = xs !! 1
+          perfVarsLsts = [xs, tail ys]
+          xs = head varsLsts
+          ys = last varsLsts
 
-coorsAreEqual :: Coors -> Coors -> Bool
-coorsAreEqual [] [] = True
-coorsAreEqual coors1 coors2
-    | len1 /= len2 = False
-    | coors1!!0 /= coors2!!0 = False
-    | otherwise = coorsAreEqual (tail coors1, tail coors2)
-    where len1 = length coors1
-          len2 = length coors2
+-- Поиск коэффициентов полинома Ньютона
+getNewtonsPolCoefs :: VarsLists -> Var -> Degree -> CoefsList
+getNewtonsPolCoefs varsLsts var degree
+    | degree < 0 = []
+    | length ys == lenght xs = (getNewtonsPolCoefs varsDiffsLsts var degree - 1) : y0
+    | otherwise = (getNewtonsPolCoefs varsDiffsLsts var degree - 1) : coef
+    where
+    coef = head ys
+    ys = last varsLsts
+    xs = head varsLsts
+    y0 = if length ys `mod` 2 == 0 then ys !! (ys `div` 2) else ynear
+    ynear = if abs (yl - y0) < abs (yr - y0) then yl else yr
+    yl = ys !! (length y `div` 2)
+    yr = ys !! (length y `div` 2 + 1)
+    varsDiffsLsts = getVarsDiffsLst varsLsts
 
-getValue :: CoorsTable -> Coors -> Double
-getValue [] [_] = 0.0
-getValue table coors
-    | coorsAreEqual $ curCoors coors = curCoorsValue
-    | otherwise = getValue (tail (table), coors)
-    where curCoors = init firstCoors
-          curCoorsValue = last firstCoors
-          firstCoors = table!!0
+-- Значение полинома Ньютона при данном значении
+getNewtonsPolValue :: CoefsList -> VarsList -> Var -> Double
+getNewtonsPolValue coefs args arg degree
+    | length coefs == 1 = curCoef
+    | otherwise = curCoef * mult args arg + getNewtonsPolValue (tail coefs, init args, arg)
+    where
+    curCoef = head coefs
+    mult [] _ = 1
+    mult args arg = (arg - argi) * mult (tail args, arg)
+    argi = head args
 
-getFirstCoors :: SymmCoorsLists -> Coors
-getFirstCoors [] = []
-getFirstCoors lsts = coor : getFirstCoors $ tail lsts
-    where coor = (lsts!!0)!!0
+-- Интерполяция с помощью полинома Ньютона
+newtonsInterpol :: VarsLists -> Var -> Degree -> Double
+newtonsInterpol [] _ _ = 0.0
+newtonsInterpol varsLsts var degree = getNewtonsPolValue coefs xs var
+    where
+    coefs = getNewtonsPolCoefs varsLsts var degree
+    xs = head varsLsts
 
-getLstsWithoutFstCoors :: SymmCoorsLists -> SymmCoorsLists
-getLstsWithoutFstCoors [] = []
-getLstsWithoutFstCoors lsts = (tail (lsts!!0)) : nextCoors
-    where nextCoors = getLstsWithoutFstCoors $ tail lsts
+-- Значение функции при данных значениях переменных
+getFuncValue :: CoorsTable -> Coors -> Double
+getFuncValue [] [_] = 0.0
+getFuncValue table coors
+    | coors == curCoors = last curCoors
+    | otherwise = getFuncValue (tail table, coors)
 
-getSymmValuesList :: CoorsTable -> SymmCoorsLists -> CoorsList
-getSymmValuesList table lsts
-    | null lsts!!0 = []
-    | otherwise = curCoorsValue : nextValuesLst
-    where curCoorsValue = getValue $ table curCoors
-          curCoors = getFirstCoors lsts
-          nextValuesLst = getSymmValuesList (table, newLsts)
-          newLsts = getLstsWithoutFstCoors lsts
+-- Получение списка значений интерполяций
+getInterValuesLst :: CoorsTable -> VarsLsts -> Coors -> Degrees -> Coors -> ValuesList
+getInterValuesLst table (varLst:othVarsLsts) (var:othVars) (degree:othDegrees) curCoors
+    | null othVarsLsts = map interpolateValues varLst
+    | otherwise = map interpolateInterpolated varLst
+    where
+    interpolateValues = \v -> newtonsInterpol [varLst, valuesLst] var degree
+    interpolateInterpolated = \v -> newtonsInterpol [varLst, getInterpolLst v] var degree
+    valuesLst = map getValue varLst
+    getValue = \v -> getFuncValue (table, curCoors : v)
+    getInterpolLst = \v -> getInterValuesLst (table, othVarsLsts, othVars, othDegrees, curCoors : v)
 
-getSymmNodes :: CoorsTable -> Coors -> Degrees -> SymmCoorsLists
-getSymmNodes table coors degrees = symmCoors ++ valuesList
-    where symmCoors = getSymmCoorsLists $ table coors degrees
-          valuesList = getSymmValuesList $ table symmCoors
+-- Интерполяция функции от двух пременных
+twoVarsInterpol :: CoorsTable -> VarsLists -> Coors -> Degrees -> Double
+twoVarsInterpol table (varLst:othVarsLsts) (var:othVars) (degree:othDegrees) = interpolResult
+    where
+    interpolResult = newtonsInterpol (interpolTable, var, degree)
+    interpolTable = [varLsts, valuesLst]
+    valuesLst = if length othVarsLsts == 1 then interValuesLst else othVarsLsts
+    interValuesLst = getInterValuesLst table othVarsLsts othVars othDegrees (var)
 
-variableInterpolation :: CoorsLists -> Coors -> Degrees -> Double
+getSymmLst :: VarsList -> Var -> Degree -> VarsList
+getSymmLst vars var degree
+    | length vars == degree + 1 = vars
+    | length vars <= degree = []
+    | vars !! mid < var = getSymmLst (tail vars, var, degree)
+    | otherwise = getSymmLst (init vars, var, degree)
+    where mid = length vars / 2
 
-interpolation :: CoorsTable -> Coors -> Degrees -> Double
-interpolation [] [_] [_] = 0.0
-interpolation [_] [] [_] = 0.0
-interpolation [_] [_] [] = 0.0
-interpolation table coors degrees
-    | length degrees /= length coors = 0.0
-    | otherwise = variableInterpolation $ symmNodes coors degrees
-    where symmNodes = getSymmCoorsLists $ table coors
+-- Поиск симметричных узлов
+getSymmVarsLsts :: CoorsTable -> Coors -> Degrees -> VarsLists
+getSymmVarsLsts table (coor:coors) (degree:degrees)
+    | length (head table) == 1 = []
+    | otherwise = symmLst ++ othSymmLsts
+    where
+    symmLst = getSymmLst fstVarLst var degree
+    othSymmLsts = getSymmVarsLsts tableWithoutFstVar coors degrees
+    fstVarLst = sort (nub (map head table))
+    tableWithoutFstVar = map tail table
+
+-- Многомерная интерполяция
+multVarInterpol :: CoorsTable -> Coors -> Degrees -> Double
+multVarInterpol [] [_] [_] = 0.0
+multVarInterpol [_] [] [_] = 0.0
+multVarInterpol [_] [_] [] = 0.0
+multVarInterpol table coors degrees
+    | length coors /= length degrees = 0.0
+    | length table < 2 = 0.0
+    | otherwise = twoVarsInterpol table symmVarsLsts coors degrees
+    where
+    symmVarsLsts = getSymmVarsLsts table coors degrees
+
--- lab_02/src/Main.hs
+++ lab_02/src/Main.hs
@@ -1,8 +1,32 @@
 module Main where
 
-import Data
-import Interpol
+import CoorsTable (parseDataTable)
+import Interpol (multVarInterpol)
 
 main :: IO()
 main = do
-    putStr "Введите x: "
+    file = openFile "data.txt" ReadMode
+    text = hGetContents file
+
+    let table = parseDataTable text
+
+    putStrLn "Введите x: "
+    x <- getLine
+    putStrLn "Введите y: "
+    y <- getLine
+    putStrLn "Введите z: "
+    z <- getLine
+
+    putStrLn "Введите nx: "
+    nx <- getLine
+    putStrLn "Введите ny: "
+    ny <- getLine
+    putStrLn "Введите nz: "
+    nz <- getLine
+
+    let coors = [x, y, z]
+    let degrees = [nx, ny, nz]
+
+    putStr "Результат: "
+    print (multVarInterpol table coors degrees)
+
