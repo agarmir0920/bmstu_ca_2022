4bc6716d19de17081a97729c0941d9d2234aa9be
agarmir0920 <tsarevantuan@yandex.ru>
2022-05-19 15:27:49 +0300

    Corrected SLE module

--- lab_04/SLE.hs
+++ lab_04/SLE.hs
@@ -8,14 +8,14 @@ type Roots = [Double]
 
 getCramersRoots :: Matrix Double -> Double -> Int -> Roots
 getCramersRoots mtrx det ind
-    | ind >= ncols cols = []
+    | ind >= (ncols cols - 1) = []
     | otherwise = [curDet / det] ++ getCramerRoots mtrx det (ind + 1)
     where
     curDet = detLU mtrxWithBs
     mtrxWithBs = leftPart <|> Bs <|> rightPart
-    leftPart = submatrix 1 (ind + 1) 1 rows
-    rightPart = submatrix (ind + 3) 1 rows
-    Bs = colVector (getCol ind mtrx)
+    leftPart = submatrix 1 rows 1 (ind + 1) mtrx
+    rightPart = submatrix 1 rows (ind + 3) (cols - 1) mtrx
+    Bs = colVector $ getCol cols mtrx
 
     cols = ncols mtrx
     rows = nrows mtrx
@@ -25,5 +25,7 @@ getRoots mtrx
     | det == 0 = []
     | otherwise = getCramersRoots mtrx det 0
     where
-    det = detLU mtrx
+    det = detLU $ submatrix 1 rows 1 (cols - 1) mtrx
+    rows = nrows mtrx
+    cols = ncols mtrx
 
